f69552975625c05fc53086a336ea9441d3a0a223
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-07-01 16:11:54 -0600

    Petición http

---
+++ src/interfaces/pokemon-list.response.ts
@@ -0,0 +1,11 @@
+export interface PokemonListResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: Pokemon[];
+}
+
+export interface Pokemon {
+  name: string;
+  url: string;
+}
